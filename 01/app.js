aa4f503a5c8ad62162e8d7d4aa2057340dec1573
ltsme <guswns2415@naver.com>
2020-03-29 18:22:13 +0900

    01 수업 과제

---
+++ 01/app.js
@@ -0,0 +1,18 @@
+console.log('app.js');
+
+var age = 20;
+var isTrue = true;
+
+month = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+];
+
+var Student_1 = {
+    age: 19,
+    gender: 'male'
+};
+
+var Student_2 = {
+    age: 19,
+    gender: 'female'
+};
